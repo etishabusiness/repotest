d9c7f1d55ff16d3f62c5ba9a92ea7ee90fbdcd06
kaushlendra <kaushlendra@gmail.com>
2016-03-31 12:10:31 +0530

    gg

--- PythonTest/com/etisha/test/__init__.py
+++ PythonTest/com/etisha/test/__init__.py
@@ -2,7 +2,7 @@ from nsepy.archives import get_price_history
 
 
 dates = [('01-01-2015', '31-01-2015'), ('01-04-2015', '31-07-2015'),
-         ('01-07-2015', '30-10-2015'), ('01-10-2015', '31-12-2015')]
+         ('01-07-2015',   '30-10-2015'), ('01-10-2015', '31-12-2015')]
 
 for date in dates:
     df = get_price_history(stock = 'DABUR', start = date[0], end = date[1])
